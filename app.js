0111177314253aeeb07aff3bf563bbe3081f3b53
muhcow <cmuh@student.unimelb.edu.au>
2019-07-04 14:41:21 +1000

    ejs working app set appset app use

---
+++ app.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var app = express();
+var path = require('path');
+//var ejs = require('ejs');
+app.use(express.static(__dirname + '/views'));
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT,()=>console.log(`listening on ${PORT}`));
+
+app.get("/",function(req,res){
+    //res.send("heyo wuddup");
+    //res.send("assets/listPage.html");
+    //res.sendFile(path.join(__dirname + '/assets/listPage.html'));
+    res.render('listPage',{listItem:'yoyo'});
+}); 
+
+console.log("yo wuddup");
