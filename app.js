658c0fc28eae3a44c37d593fa106e172c3b735a5
muhcow <cmuh@student.unimelb.edu.au>
2019-07-13 21:17:22 +0200

    jikan api connection

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var app = express();
 var path = require('path');
 app.use(express.static(__dirname + '/views'));
@@ -7,7 +8,10 @@ app.set('view engine', 'ejs');
 const PORT = process.env.PORT || 3000;
 const routes = require('./routes/routes.js');
 const db = require('./database/db.js');
-
+app.use(bodyParser.json()); // support json encoded bodies
+app.use(bodyParser.urlencoded({
+    extended: true
+  }));
 app.use('/',routes);
 
 app.listen(PORT,()=>console.log(`listening on ${PORT}`));
--- routes/routes.js
+++ routes/routes.js
@@ -1,10 +1,26 @@
 var express = require('express');
 var router = express.Router();
+const request = require('request');
 
 //homepage route
 router.get("/",function(req,res){
     var listItems = ["hi","ok","wow"];
     res.render('listPage',{listItems:listItems});
 }); 
+
+//return search results route
+router.post("/search",function(req,res){
+    console.log(req.body);
+    var animeName = req.body.searchBar;
+    var searchResults;
+    //jikan api request
+    request(`https://api.jikan.moe/v3/search/anime/1/?q=${animeName}`, { json: true }, (err, res, body) => {
+        if (err) { return console.log(err); }
+            //only return top 3 search results
+            searchResults = body.results.slice(0,3);
+            console.log(searchResults);
+        });
+    res.send(req.body.searchBar);
+});
 
 module.exports = router;
